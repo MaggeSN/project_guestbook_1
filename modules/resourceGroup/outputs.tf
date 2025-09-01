d2dcc824f0588f9eb7fb589b3514ca262105862a
fredrik <fredrik.flaskjer@icloud.com>
2025-09-01 14:40:09 +0200

    created resource for rg, with outputs

---
+++ modules/resourceGroup/outputs.tf
@@ -0,0 +1,8 @@
+output "name"{
+    value = azurerm_resource_group.rg.name
+    description = "Name of the resource group"
+}
+output "location"{
+    value = azurerm_resource_group.rg.location
+    description = "Location of the resource group"
+}
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "rg_name" {
+    value       = module.resourceGroup.name
+    description = "Name of the resource group"
+}
+output "location" {
+    value       = module.resourceGroup.location
+    description = "Location of the resource group"
+}
