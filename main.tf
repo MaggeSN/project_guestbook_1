f134affdfee761bbb0d1d50188bbe222105c36dc
fredrik <fredrik.flaskjer@icloud.com>
2025-09-02 14:04:23 +0200

    wdwdwdwdw

--- main.tf
+++ main.tf
@@ -81,10 +81,6 @@ import {
   id = "/subscriptions/${var.SUBSCRIPTION_ID}/resourceGroups/guestbook_rg_2/providers/Microsoft.App/containerApps/backend-app"
 }
 import {
-  to = module.containers.azurerm_container_app_environment.container_environment
-  id = "/subscriptions/${var.SUBSCRIPTION_ID}/resourceGroups/guestbook_rg_2/providers/Microsoft.App/managedEnvironments/container-environment"
-}
-import {
   to = module.cosmosDB.azurerm_cosmosdb_account.db_account
   id = "/subscriptions/${var.SUBSCRIPTION_ID}/resourceGroups/guestbook_rg_2/providers/Microsoft.DocumentDB/databaseAccounts/tfex-cosmos-db-account-1"
 }
