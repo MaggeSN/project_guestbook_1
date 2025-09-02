7c36e182f5dd0d092375f3a49d743b4666e6c026
fredrik <fredrik.flaskjer@icloud.com>
2025-09-02 10:15:27 +0200

    prøva aigjen igjen igjen

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "backend_storage_account" {
-  name                     = "backendstorageaccount4242"
+  name                     = "backendstorageaccount${random_integer.suffix.result}"
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
