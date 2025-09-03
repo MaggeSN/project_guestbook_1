e5b218558d1403c3b34cf3f482c1da22d18e41f5
fredrik <fredrik.flaskjer@icloud.com>
2025-09-03 10:55:20 +0200

    endra submask for container net

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "containerapps" {
   name                 = "subnet-containerapps"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/23"]
   delegation {
     name = "delegation"
     service_delegation {
