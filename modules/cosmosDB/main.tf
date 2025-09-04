c83a76e6accf3babfc1bdab49ebea2d8ff0534e0
fredrik <fredrik.flaskjer@icloud.com>
2025-09-04 12:45:02 +0200

    dwdwwdwddw

--- modules/cosmosDB/main.tf
+++ modules/cosmosDB/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mongo_cluster" "cluster" {
-  name                   = "cosmosdb_cluster"
+  name                   = "cluster"
   resource_group_name    = var.resource_group_name
   location               = var.location
   administrator_username = "adminTerraform"
