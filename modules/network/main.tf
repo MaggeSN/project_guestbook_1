c61fb77acddd4e267e4c5b4b7ce57c12bf37f781
fredrik <fredrik.flaskjer@icloud.com>
2025-09-04 09:47:25 +0200

    saewfwrqfwfd

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "cosmosdb" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-  private_endpoint_network_policies = "enabled"
+  private_endpoint_network_policies = "Enabled"
 }
 
 resource "azurerm_private_endpoint" "db_endpoint" {
