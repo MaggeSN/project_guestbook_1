2818d6a1df93ac1f407569fbee74331e4ec8671b
Magnus Santos Nordeide <magnus.nordeide@soprasteria.com>
2025-09-05 10:05:37 +0200

    one line

--- modules/containers/main.tf
+++ modules/containers/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_container_app" "frontend_container" {
       }
 
       env {
-        name  = "AUTH_TOKEN_FRONTEND"
+        name  = "AUTH_TOKEN"
         value = var.AUTH_TOKEN
       }
     }
